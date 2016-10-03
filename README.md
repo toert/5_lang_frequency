686f26e8b170efa161b89cc4f04c18ccb542c840
toert <pw177@mail.ru>
2016-10-03 12:53:37 +0400

    на проверку

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 5_lang_frequency
+
+The script finds the most frequent words in your text. It works with .txt files. 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,24 @@
+import re
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as data:
+        return data.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    words = get_all_words(text)
+    cntr = Counter(words)
+    print(cntr.most_common(10))
+
+
+def get_all_words(text):
+    words = re.findall(r'[^\W|\d]+', text) #De Morgan's law
+    return words
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input('Name of the file:')
+    text_from_file = load_data(filepath)
+    get_most_frequent_words (text_from_file)
