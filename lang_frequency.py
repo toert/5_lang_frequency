d5fecc4b2fc3cd4b5a3b2164fdc992d756fb8808
toert <pw177@mail.ru>
2016-10-04 19:58:42 +0400

    исправлены ошибки оформления

--- lang_frequency.py
+++ lang_frequency.py
@@ -8,17 +8,23 @@ def load_data(filepath):
 
 
 def get_most_frequent_words(text):
+    
     words = get_all_words(text)
     cntr = Counter(words)
-    print(cntr.most_common(10))
-
+    number_of_output_words = 10
+    list_of_common_words = cntr.most_common(number_of_output_words)
+    print(str(number_of_output_words) + ' of the most common words: ')
+    for number_of_word in range(number_of_output_words):
+        print(list_of_common_words[number_of_word])
+        
 
 def get_all_words(text):
-    words = re.findall(r'[^\W|\d]+', text) #De Morgan's law
+    words = re.findall(r'[^\W|\d]+', text)
     return words
 
 
 if __name__ == '__main__':
     filepath = input('Name of the file:')
     text_from_file = load_data(filepath)
+    text_from_file.lower()
     get_most_frequent_words (text_from_file)
