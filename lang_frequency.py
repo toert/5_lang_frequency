15a25c060065a9031e0663161a0712d12fa7731e
toert <pw177@mail.ru>
2016-10-06 10:18:25 +0400

    function was getting pure

--- lang_frequency.py
+++ lang_frequency.py
@@ -7,15 +7,12 @@ def load_data(filepath):
         return data.read()
 
 
-def get_most_frequent_words(text):
+def get_most_frequent_words(text,number_of_words_to_output):
     
     words = get_all_words(text)
     cntr = Counter(words)
-    number_of_output_words = 10
-    list_of_common_words = cntr.most_common(number_of_output_words)
-    print(str(number_of_output_words) + ' of the most common words: ')
-    for number_of_word in range(number_of_output_words):
-        print(list_of_common_words[number_of_word])
+    list_of_common_words = cntr.most_common(number_of_words_to_output)
+    return list_of_common_words
         
 
 def get_all_words(text):
@@ -24,7 +21,11 @@ def get_all_words(text):
 
 
 if __name__ == '__main__':
+    number_of_output_words = 10
     filepath = input('Name of the file:')
     text_from_file = load_data(filepath)
     text_from_file.lower()
-    get_most_frequent_words (text_from_file)
+    list_of_common_words = get_most_frequent_words (text_from_file, number_of_output_words)
+    print(str(number_of_output_words) + ' of the most common words: ')
+    for number_of_word in range(number_of_output_words):
+        print(list_of_common_words[number_of_word])
