0b882e858753a29dd97486bce0bdabffb9480ca5
toert <pw177@mail.ru>
2016-10-07 15:58:03 +0400

    поменял названия переменных и изменил метод конкатенации

--- lang_frequency.py
+++ lang_frequency.py
@@ -7,13 +7,13 @@ def load_data(filepath):
         return data.read()
 
 
-def get_most_frequent_words(text,number_of_words_to_output):
-    
+def get_most_frequent_words(text, number_of_words_to_output):
     words = get_all_words(text)
-    cntr = Counter(words)
-    list_of_common_words = cntr.most_common(number_of_words_to_output)
+    counter_words = Counter(words)
+    list_of_common_words = \
+        counter_words.most_common(number_of_words_to_output)
     return list_of_common_words
-        
+
 
 def get_all_words(text):
     words = re.findall(r'[^\W|\d]+', text)
@@ -25,7 +25,9 @@ if __name__ == '__main__':
     filepath = input('Name of the file:')
     text_from_file = load_data(filepath)
     text_from_file.lower()
-    list_of_common_words = get_most_frequent_words (text_from_file, number_of_output_words)
-    print(str(number_of_output_words) + ' of the most common words: ')
-    for number_of_word in range(number_of_output_words):
-        print(list_of_common_words[number_of_word])
+    list_of_common_words = get_most_frequent_words(text_from_file, \
+                                                   number_of_output_words)
+    print('{} {}'.format(str(number_of_output_words), \
+                         'of the most common words: '))
+    for word in list_of_common_words:
+        print(word)
